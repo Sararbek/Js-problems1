688a904bc331528b5319f07ea47ebf929022b1bb
sararbek <serkinov393@gmail.com>
2024-11-02 14:36:07 +0500

    js problems

---
+++ for.js
@@ -0,0 +1,83 @@
+{
+    // 1-masala
+    let k = 5, n = 6
+    for(let i = 0; i <= n; i++){
+        console.log(k)
+    }
+}
+
+{
+    // 2-masala
+    let a = 5, b = 10
+    for(let i = a; i <= b; i++){
+        console.log(i)
+    }
+}
+
+{
+    //3-masala
+    let a = 5, b = 10
+    for(let i = b - 1; i > a; i--){
+        console.log(i)
+    }
+}
+
+{
+    //4-masala
+    let sweetPrice = 2
+    for(let i = 1; i <= 10; i++){
+        console.log(sweetPrice * i)
+    }
+}
+
+{
+    //5-masala
+    let sweetPrice = 2
+    for(let i = 1; i <= 10; i++){
+        console.log((sweetPrice * i) / 10)
+    }
+}
+
+{
+    // 6-masala
+    let sweetPrice = 2
+    for(let i = 12; i <= 20; i++){
+        console.log((sweetPrice * i) / 10)
+    }
+}
+
+{
+    //7-masala
+    let a = 5, b = 10, sum = 0
+    for(let i = a; i <= b; i++){
+        sum += i
+    }
+    console.log(sum)
+}
+
+{
+    //8-masala
+    let a = 5, b = 10, sum = 1
+    for(let i = a; i <= b; i++){
+        sum *= i
+    }
+    console.log(sum)
+}
+
+{
+    //9-masala
+    let a = 5, b = 6, sum = 0
+    for(let i = a; i <= b; i++){
+        sum += i**2
+    }
+    console.log(sum)
+}
+
+{
+    //10-masala
+    let n = 4, sum = 0
+    for(let i = 1; i <= n;  i++){
+        sum += 1 / i
+    }
+    console.log(sum)
+}
